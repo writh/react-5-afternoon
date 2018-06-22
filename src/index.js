97372f3ab690839bfdf239c0f3023590f4dfc104
Clifford Christensen <victoria.thursday@gmail.com>
2018-06-21 21:01:14 -0600

    finished step 4

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { HashRouter } from 'react-router-dom';
+import store from './store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+    <HashRouter>
+        <App />
+    </HashRouter>
+</Provider>
+, document.getElementById('root'));
 registerServiceWorker();
-
